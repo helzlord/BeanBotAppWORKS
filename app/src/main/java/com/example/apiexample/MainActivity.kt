2ba7fb4a794d41b3df1ef350bb414c58fa241d2c
Rohan Helewaut <rohanhelewaut@gmail.com>
2023-04-24 12:32:49 +0200

    a

--- app/src/main/java/com/example/apiexample/MainActivity.kt
+++ app/src/main/java/com/example/apiexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.apiexample
 
+import android.content.ClipData
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,14 +10,17 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
@@ -28,6 +33,7 @@ import com.example.apiexample.ui.theme.ApiExampleTheme
 import com.example.apiexample.ui.theme.Purple700
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,77 +64,170 @@ data class UserModel(
 
 @Composable
 fun MainScreen() {
-   Scaffold(
-       topBar = {
-           TopAppBar(
-               backgroundColor = Purple700,
-               title = {
-                   Text(
-                       text = "Simple API Request",
-                       modifier = Modifier.fillMaxWidth(),
-                       textAlign = TextAlign.Center,
-                       color = Color.White
-                   )
-               }
-           )
-       },
-       content = {
-          Column(
-              modifier = Modifier.fillMaxWidth(),
-              verticalArrangement = Arrangement.Center,
-              horizontalAlignment = Alignment.CenterHorizontally
-          ) {
-              val id = remember {
-                  mutableStateOf(TextFieldValue())
-              }
+    var RodeBonen = "70%"
+    var ZwarteBonen = "50%"
+    var WitteBonen = "69%"
+    var RodeWil = 0
+    var ZwarteWil = 0
+    var WitteWil = 0
+    var sliderPosition by remember { mutableStateOf(150f) }
+    var gewichtBonen = sliderPosition.toInt()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Purple700,
+                title = {
+                    Text(
+                        text = "Simple API Request",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val id = remember {
+                    mutableStateOf(TextFieldValue())
+                }
 
-              val profile = remember {
-                  mutableStateOf(ProfileModel(
-                      age = "",
-                      name = "",
-                      email = ""
-                  ))
-              }
+                val profile = remember {
+                    mutableStateOf(
+                        ProfileModel(
+                            age = "",
+                            name = "",
+                            email = ""
+                        )
+                    )
+                }
 
-              Text(
-                  text="API Sample",
-                  style= TextStyle(
-                      fontSize = 40.sp,
-                      fontFamily = FontFamily.Cursive
-                  )
-              )
+                Text(
+                    text = "API Sample",
+                    style = TextStyle(
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily.Cursive
+                    )
+                )
 
-              Spacer(modifier = Modifier.height(15.dp))
+                Spacer(modifier = Modifier.height(15.dp))
 
-              TextField(
-                  label = { Text(text = "User ID")},
-                  value = id.value,
-                  onValueChange = { id.value = it }
-              )
+                TextField(
+                    label = { Text(text = "User ID") },
+                    value = id.value,
+                    onValueChange = { id.value = it }
+                )
 
-              Spacer(modifier = Modifier.height(15.dp))
+                Spacer(modifier = Modifier.height(15.dp))
 
-              Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-                  Button(
-                      onClick = {
-                          val data = sendRequest(
-                              id = id.value.text,
-                              profileState = profile
-                          )
+                Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                    Button(
+                        onClick = {
+                            val data = sendRequest(
+                                id = id.value.text,
+                                profileState = profile
+                            )
 
-                          Log.d("Main Activity", profile.toString())
-                      }
-                  ) {
-                      Text(text = "Get Data")
-                  }
-              }
-              
-              Spacer(modifier = Modifier.height(15.dp))
-              
-              Text(text = profile.component1().toString(), fontSize = 40.sp)
-          }
-       }
-   )
+                            Log.d("Main Activity", profile.toString())
+                        }
+                    ) {
+                        Text(text = "Get Data")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(text = profile.component1().toString(), fontSize = 40.sp)
+            }
+        }
+    )
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    )
+    {
+        item{Text(text = RodeBonen, fontSize = 35.sp)}
+        item {
+            Button(
+                onClick = { RodeWil++ }
+            )
+            {
+                Text(text = "Rode Bonen")
+            }
+        }
+        item {
+            Row() {
+                Text(text = ZwarteBonen, fontSize = 35.sp)
+                Spacer(modifier = Modifier.width(30.dp))
+                Button(
+                    onClick = { ZwarteWil++ }
+                )
+                {
+                    Text(text = "Zwarte Bonen")
+                }
+            }
+        }
+        item {
+            Box() {
+                Row() {
+                    Text(text = WitteBonen, fontSize = 35.sp)
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Button(
+                        onClick = { WitteWil++ },
+                    )
+                    {
+                        Text(text = "Witte Bonen")
+                    }
+                }
+            }
+        }
+        item {
+            Box() {
+                Text(
+                    text = "Duid het gewenste gewicht aan op de slider",
+                    fontSize = 18.sp
+                )
+            }
+        }
+        item {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.padding(1.dp)
+            ) {
+                Text(
+                    modifier = Modifier.width(50.dp),
+                    text = gewichtBonen.toString(),
+                    fontSize = 20.sp
+                )
+            }
+        }
+        item {
+            Slider(
+                modifier = Modifier.semantics { contentDescription = "Localized Description" },
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 150f..300f,
+                steps = 150
+            )
+        }
+        item {
+            Button(
+                onClick = {
+                    if ((RodeWil > 0) || (ZwarteWil > 0) || (WitteWil > 0)) {
+                    }
+                }
+            )
+            {
+                Text(
+                    text = "Order",
+                    fontSize = 50.sp
+                )
+            }
+        }
+    }
 }
 
 fun sendRequest(
